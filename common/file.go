821a876e89596913748843d1ce02abd23ce693cf
g278701988 <35322051+g278701988@users.noreply.github.com>
2022-02-11 15:41:50 +0800

    Update file.go

--- common/file.go
+++ common/file.go
@@ -1,1 +1,55 @@
+package common
 
+import (
+	"errors"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+// CurrentPath from Current execute path
+func CurrentPath() string {
+	var dirAbsPath string
+	ex, err := os.Executable()
+	if err == nil {
+		dirAbsPath = filepath.Dir(ex)
+		return dirAbsPath
+	}
+
+	exReal, err := filepath.EvalSymlinks(ex)
+	if err != nil {
+		return ""
+	}
+	dirAbsPath = filepath.Dir(exReal)
+	return dirAbsPath
+}
+func AppendToFile(path string, bytes []byte) error {
+	// If the file doesn't exist, create it, or append to the file
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Printf("Error closing file: %s\n", err)
+		}
+	}()
+	if _, err := f.Write(bytes); err != nil {
+		return err
+	}
+
+	return nil
+}
+func IsFileNotExist(path string) bool {
+	f, err := os.OpenFile(path, os.O_RDWR, 0666)
+	if errors.Is(err, os.ErrNotExist) {
+		return true
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Printf("Error closing file: %s\n", err)
+		}
+	}()
+	return false
+
+}
